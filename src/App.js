a766077cc4b87e80483637b4c3843e06fa821631
Zhandos <saparbayevzh03@gmail.com>
2022-06-15 00:23:03 +0600

    react-useState-hw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,179 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+
+// button-group
+const buttons = [
+  {
+    type: "all",
+    label: "All",
+  },
+  {
+    type: "active",
+    label: "Active",
+  },
+  {
+    type: "done",
+    label: "Done",
+  },
+];
+
+const toDoItems = [];
 
 function App() {
+  const [itemToAdd, setItemToAdd] = useState("");
+  const [items, setItems] = useState(() => toDoItems);
+  const [searchItem, setSearchItem] = useState("");
+  const [filterType, setFilterType] = useState("");
+
+  const handleChangeItem = (event) => {
+    setItemToAdd(event.target.value);
+  };
+
+  const handleSearchItem = (event) => {
+    setSearchItem(event.target.value);
+  }
+  console.log(searchItem);
+
+  const handleAddItem = () => {
+    setItems((prevItems) => [
+      { label: itemToAdd, key: uuidv4() },
+      ...prevItems,
+    ]);
+
+    setItemToAdd("");
+  };
+
+  const toRemoveItem = ({key}) => {
+    const itemIndex = items.findIndex((item) => item.key === key);
+  
+    const leftSideOfAnArray = items.slice(0, itemIndex);
+    const rightSideOfAnArray = items.slice(itemIndex + 1, items.length);
+
+    setItems([...leftSideOfAnArray, ...rightSideOfAnArray]);
+  }
+
+  const handleItemDone = ({ key }) => {
+    setItems((prevItems) =>
+      prevItems.map((item) => {
+        if (item.key === key) {
+          return { ...item, done: !item.done };
+        } else return item;
+      })
+    );
+  };
+
+  const handleItemImportant = ({key}) => {
+    setItems((prevItems) =>
+    prevItems.map((item) => {
+      if (item.key === key) {
+        return { ...item, important: !item.important };
+      } else return item;
+    })
+  );
+  }
+
+  const handleFilterItems = (type) => {
+    setFilterType(type);
+  };
+
+  const amountDone = items.filter((item) => item.done).length;
+
+  const amountLeft = items.length - amountDone;
+
+  const filteredItems =
+    !filterType || filterType === "all"
+      ? items
+      : filterType === "active"
+      ? items.filter((item) => !item.done)
+      : items.filter((item) => item.done);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app">
+      {/* App-header */}
+      <div className="app-header d-flex">
+        <h1>Todo List</h1>
+        <h2>
+          {amountLeft} more to do, {amountDone} done
+        </h2>
+      </div>
+
+      <div className="top-panel d-flex">
+        {/* Search-panel */}
+        <input
+          type="text"
+          className="form-control search-input"
+          placeholder="type to search"
+          onChange={handleSearchItem}
+        />
+        {/* Item-status-filter */}
+        <div className="btn-group">
+          {buttons.map((item) => (
+            <button
+              onClick={() => handleFilterItems(item.type)}
+              key={item.type}
+              type="button"
+              className={`btn btn-${
+                filterType !== item.type ? "outline-" : ""
+              }info`}
+            >
+              {item.label}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      {/* List-group */}
+      <ul className="list-group todo-list">
+      {items.length === 0 &&
+        <h3>
+          You don't have any tasks!
+        </h3>
+      }
+        {filteredItems.length > 0 &&
+          filteredItems.map((item) => (
+            <li key={item.key} className="list-group-item">
+              <span className={`todo-list-item${item.done ? " done" : ""} ${item.important ? " important": ""}`}>
+                <span
+                  className="todo-list-item-label"
+                  onClick={() => handleItemDone(item)}
+                >
+                  {item.label}
+                </span>
+
+                <button
+                  type="button"
+                  className="btn btn-outline-success btn-sm float-right"
+                  onClick={() => handleItemImportant(item)}
+                >
+                  <i className="fa fa-exclamation" />
+                </button>
+
+                <button
+                  type="button"
+                  className="btn btn-outline-danger btn-sm float-right"
+                  onClick={() => toRemoveItem(item)}
+                >
+                  <i className="fa fa-trash-o" />
+                </button>
+              </span>
+            </li>
+          ))}
+      </ul>
+
+      {/* Add form */}
+      <div className="item-add-form d-flex">
+        <input
+          value={itemToAdd}
+          type="text"
+          className="form-control"
+          placeholder="What needs to be done"
+          onChange={handleChangeItem}
+        />
+        <button className="btn btn-outline-secondary" onClick={handleAddItem}>
+          Add item
+        </button>
+      </div>
     </div>
   );
 }
